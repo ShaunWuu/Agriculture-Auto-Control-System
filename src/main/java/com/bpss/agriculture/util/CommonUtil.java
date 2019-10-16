7fdb6c9f872916996a4721b273d0b55ecce74d2e
Rocky Wu <554350076@qq.com>
2019-10-16 16:00:28 +0800

    添加 Utils 注释

--- src/main/java/com/bpss/agriculture/util/CommonUtil.java
+++ src/main/java/com/bpss/agriculture/util/CommonUtil.java
@@ -8,6 +8,10 @@ import java.util.Date;
  */
 public class CommonUtil {
 
+    /**
+     * 获取符合数据库时间格式的时间字符串
+     * @return
+     */
     public static String getVcTime(){
         Date date = new Date();
         SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMddHHmmss");
